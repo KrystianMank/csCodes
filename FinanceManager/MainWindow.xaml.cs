4f58e1a58865b0a8528eeac290e4df75bf1c253f
KrystianMank <man-kry-2005@wp.pl>
2025-05-16 17:38:44 +0200

    podstawowy widok aplikacji

--- FinanceManager/MainWindow.xaml.cs
+++ FinanceManager/MainWindow.xaml.cs
@@ -24,5 +24,36 @@ namespace FinanceManager
         {
             InitializeComponent();
         }
+
+        private void TabItem_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void MainPageTab_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            MainPageContent.Content = new MainPage();
+        }
+
+        private void Tabs_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            TabItem selectedTab = (TabItem)Tabs.SelectedItem;
+            if (selectedTab != null)
+            {
+                string tabname = selectedTab.Header.ToString();
+                if(tabname == "Strona główna")
+                {
+                    MainPageContent.Content = new MainPage().Content;   
+                }
+                else if(tabname == "Wizualizacja danych")
+                {
+                    DataVisualisationContent.Content = new DataVisualisation().Content;
+                }
+                else if(tabname == "Eksport/Import danych")
+                {
+                    SharingDataContent.Content = new SharingData().Content;
+                }
+            }
+        }
     }
 }
