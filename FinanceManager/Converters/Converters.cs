8946982fbdae75a6b628b5beb5ba796abd5aaa06
KrystianMank <man-kry-2005@wp.pl>
2025-05-18 21:25:20 +0200

    utowrzenie pliku Converters, dodanie klasy BoolToIndexConverter

---
+++ FinanceManager/Converters/Converters.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace FinanceManager.Converters
+{
+    public class BoolToIndexConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? 0 : 1; // 0 - Przychód, 1 - Wydatek
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (int)value == 0; // true - Przychód, false - wydatek
+        }
+    }
+
+    public class IsTodayConverter
+    {
+        
+    }
+}
