3a10b4a6dc9a5bd84a59c87ff7f3b73f7cc5ad25
KrystianMank <man-kry-2005@wp.pl>
2025-05-28 21:14:58 +0200

    Implementacja interfejsu INotifyProperyChanged to klasy Transaction

--- FinanceManager/Models/Transaction.cs
+++ FinanceManager/Models/Transaction.cs
@@ -1,20 +1,91 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FinanceManager.Models
 {
-    public class Transaction
+    public class Transaction : INotifyPropertyChanged
     {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public decimal Amount { get; set; }
-        public DateTime Date { get; set; }
-        public string Description { get; set; }
-        public bool IsIncome { get; set; }
-        public string Source { get; set; }
+        private int _id;
+        private string _title;
+        private decimal _amount;
+        private DateTime _date;
+        private string _description;
+        private bool _isIncome;
+        private string _source;
+        public int Id
+        {
+            get => _id;
+            set
+            {
+                _id = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                _title = value;
+                OnPropertyChanged();
+            }
+        }
+        public decimal Amount
+        {
+            get => _amount;
+            set
+            {
+                _amount = value;
+                OnPropertyChanged();
+            }
+        }
+        public DateTime Date
+        {
+            get => _date;
+            set
+            {
+                _date = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Description
+        {
+            get => _description;
+            set
+            {
+                _description = value;
+                OnPropertyChanged();
+            }
+        }
+        public bool IsIncome
+        {
+            get => _isIncome;
+            set
+            {
+                _isIncome = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Source
+        {
+            get => _source;
+            set
+            {
+                _source = value;
+                OnPropertyChanged();
+            }
+        }
 
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
