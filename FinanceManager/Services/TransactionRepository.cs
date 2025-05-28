d3d0365ea3b1c7843ecacf3dd92ff4c7a7197f30
KrystianMank <man-kry-2005@wp.pl>
2025-05-28 21:16:00 +0200

    Dodanie metod do klasy TransactionRepository

--- FinanceManager/Services/TransactionRepository.cs
+++ FinanceManager/Services/TransactionRepository.cs
@@ -14,7 +14,7 @@ namespace FinanceManager.Services
         // Metoda do pobierania wszystkich transakcji z bazy danych
         public IEnumerable<Transaction> GetAll()
         {
-            using (var connection = new NpgsqlConnection("Host=localhost;Username=postgres;Password=postgres;Database=transactionsdatabase"))
+            using (var connection = new NpgsqlConnection(connectionString))
             {
                 connection.Open();
                 using (var command = new NpgsqlCommand("SELECT * FROM transactions", connection))
@@ -37,18 +37,19 @@ namespace FinanceManager.Services
             }
         }
 
+        // Metoda do dodawania nowej transakcji do bazy danych
         public void AddTransaction(Transaction transaction)
         {
-            using (var connection = new NpgsqlConnection("Host=localhost;Username=postgres;Password=postgres;Database=transactionsdatabase"))
+            using (var connection = new NpgsqlConnection(connectionString))
             {
                 connection.Open();
-                using (var command = new NpgsqlCommand("INSERT INTO transactions (title, amount, date, description, is_income, source) VALUES (@title, @amount, @date, @description, @is_income, @source)", connection))
+                using (var command = new NpgsqlCommand("INSERT INTO transactions (title, amount, date, description, type, source) VALUES (@title, @amount, @date, @description, @type, @source)", connection))
                 {
                     command.Parameters.AddWithValue("title", transaction.Title);
                     command.Parameters.AddWithValue("amount", transaction.Amount);
                     command.Parameters.AddWithValue("date", transaction.Date);
                     command.Parameters.AddWithValue("description", transaction.Description);
-                    command.Parameters.AddWithValue("is_income", transaction.IsIncome);
+                    command.Parameters.AddWithValue("type", transaction.IsIncome);
                     command.Parameters.AddWithValue("source", transaction.Source);
                     command.ExecuteNonQuery();
                 }
