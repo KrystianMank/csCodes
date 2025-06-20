09477502f147b97e0c271ff04f2fc77e884bbec1
KrystianMank <man-kry-2005@wp.pl>
2025-06-20 15:20:08 +0200

    Poprzedni stan aplikacji

---
+++ ToDoListWebApp/Migrations/20250620124744_PastDueTaskItems_table.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDoListWebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class PastDueTaskItems_table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_TaskItems",
+                table: "TaskItems");
+
+            migrationBuilder.RenameTable(
+                name: "TaskItems",
+                newName: "TaskItem");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_TaskItem",
+                table: "TaskItem",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_TaskItem",
+                table: "TaskItem");
+
+            migrationBuilder.RenameTable(
+                name: "TaskItem",
+                newName: "TaskItems");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_TaskItems",
+                table: "TaskItems",
+                column: "Id");
+        }
+    }
+}
