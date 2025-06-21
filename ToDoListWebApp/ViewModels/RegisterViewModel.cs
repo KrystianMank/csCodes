122ba7667fd65686f2b0da9773ac4466ba094700
KrystianMank <man-kry-2005@wp.pl>
2025-06-21 18:36:21 +0200

    Utworzenie viewmodeli dla logowania i rejestracji

---
+++ ToDoListWebApp/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoListWebApp.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Password must contain at least {2} and at most {1} characters", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords don't match")]
+        [Display(Name = "Confirm password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
