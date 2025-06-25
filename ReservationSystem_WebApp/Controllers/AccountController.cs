40fa9aae998b42b7049c843a1e2ec28aa51e7242
KrystianMank <man-kry-2005@wp.pl>
2025-06-25 19:24:51 +0200

    Kontroler AccountController

--- ReservationSystem_WebApp/Controllers/AccountController.cs
+++ ReservationSystem_WebApp/Controllers/AccountController.cs
@@ -1,16 +1,78 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ReservationSystem_WebApp.Models;
+using ReservationSystem_WebApp.ViewModels;
+using ReservationSystem_WebApp.Data;
+using System.Threading.Tasks;
 
 namespace ReservationSystem_WebApp.Controllers
 {
     public class AccountController : Controller
     {
+        private UserManager<User> _userManager;
+        private SignInManager<User> _signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
         public IActionResult Login()
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(
+                    model.Email, model.Password, model.RemeberMe, lockoutOnFailure: false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                ModelState.AddModelError(string.Empty, "Failed login attempt");
+            }
+            return View(model);
+        }
+
+        [HttpGet]
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new User { UserName =  model.Email, Email = model.Email, AccessType = ApplicationData.AccessType.Client };
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Login", "Account");
         }
     }
 }
