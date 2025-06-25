552357acc75923b6a2dec51ad007fff99435db71
KrystianMank <man-kry-2005@wp.pl>
2025-06-25 19:11:11 +0200

    Dodanie ViewModeli logowania i rejestracji

---
+++ ReservationSystem_WebApp/ViewModels/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReservationSystem_WebApp.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remeber Me")]
+        public bool RemeberMe { get; set; }
+    }
+}
---
+++ ReservationSystem_WebApp/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReservationSystem_WebApp.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        [StringLength(100, ErrorMessage = "Password must contain at least {2} and at most {1} characters", MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password don't match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
