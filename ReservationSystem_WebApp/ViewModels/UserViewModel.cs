6cb9d4eb6055967f5ced4e3ed14a27c78ca61a4e
KrystianMank <man-kry-2005@wp.pl>
2025-06-30 17:09:30 +0200

    ViewModel - użytkownik

---
+++ ReservationSystem_WebApp/ViewModels/UserViewModel.cs
@@ -0,0 +1,24 @@
+﻿using ReservationSystem_WebApp.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace ReservationSystem_WebApp.ViewModels
+{
+    public class UserViewModel
+    {
+        public string? Id { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string? Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string? Password { get; set; }
+
+        [Required]
+        [Display(Name = "Access Type")]
+        public ApplicationData.AccessType AccessType { get; set; }
+    }
+}
