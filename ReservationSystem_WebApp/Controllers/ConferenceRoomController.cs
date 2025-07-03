fd69018de665695216c9693580edd02a76834f7c
KrystianMank <man-kry-2005@wp.pl>
2025-07-03 18:20:11 +0200

    Podstrona - szczegóły sali

---
+++ ReservationSystem_WebApp/Controllers/ConferenceRoomController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ReservationSystem_WebApp.Services;
+
+namespace ReservationSystem_WebApp.Controllers
+{
+    [Authorize]
+    public class ConferenceRoomController : Controller
+    {
+        private readonly IConferenceRoomService _roomService;
+        private readonly ILogger<ConferenceRoomController> _logger;
+        public ConferenceRoomController
+            (IConferenceRoomService conferenceRoomService, 
+            ILogger<ConferenceRoomController> logger)
+        {
+            _roomService = conferenceRoomService;
+            _logger = logger;
+        }
+
+        public IActionResult ConferenceRoomsList()
+        {
+            var conferenceRooms = _roomService.GetConferenceRoomsList();
+            return View(conferenceRooms);
+        }
+        public IActionResult ConferenceRoomDetail(int roomId)
+        {
+            var (room, errorMessage) = _roomService.GetConferenceRoom(roomId);
+            if(room != null)
+            {
+                return View(room);
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, errorMessage);
+                return RedirectToAction("ConferenceRoomsList", "ConferenceRoom");
+            }
+            
+        }
+    }
+}
